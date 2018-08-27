c6a366c9ba1c1d39a99916496a3ea78ff396dbba
Cesos1104 <cesos1104@hotmail.com>
2018-08-26 19:23:05 -0500

    JavaScript de las cards close

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,28 +3,34 @@ function showmodal(boton){
 	switch(boton){
 		case "BtnSV":
 		var modal = document.getElementById('SVModal');
+		var span = document.getElementsByClassName("close")[0];	
 		break;
 		case "BtnSD":
 		var modal = document.getElementById('SDModal');
+		var span = document.getElementsByClassName("close")[1];	
 		break;
 		case "BtnVyS":
 		var modal = document.getElementById('VySModal');
+		var span = document.getElementsByClassName("close")[2];	
 		break;
 		case "BtnE":
 		var modal = document.getElementById('EModal');
+		var span = document.getElementsByClassName("close")[3];	
 		break;
 		case "BtnTIyST":
 		var modal = document.getElementById('TIySTModal');
+		var span = document.getElementsByClassName("close")[4];	
 		break;
 		case "BtnCyA":
 		var modal = document.getElementById('CyAModal');
+		var span = document.getElementsByClassName("close")[5];	
 		break;
 		default:
         text = "I have never heard of that fruit...";
 	}
 	modal.style.display = "block";
 	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];	
+	
 	// When the user clicks on <span> (x), close the modal
 	span.onclick = function() {
 		modal.style.display = "none";
