b3a336ad747981d692ef4abeb22256c704a88db8
Cesos1104 <cesos1104@hotmail.com>
2018-08-29 19:15:58 -0500

    mobile

--- assets/js/modalinicio.js
+++ assets/js/modalinicio.js
@@ -1,5 +1,5 @@
 //function
-function showmodal(boton){
+/*function showmodal(boton){
 	
 	switch(boton){
 		case "BtnUNAM":
@@ -50,4 +50,20 @@ function showmodal(boton){
 			modal.style.display = "none";
 		}
 	}
-}
+}*/
+
+ document.getElementsByClassName("tablink")[0].click();
+
+  function openCity(evt, cityName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].classList.remove("w3-light-grey");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.classList.add("w3-light-grey");
+  }
